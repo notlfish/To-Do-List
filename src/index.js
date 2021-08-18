25f783b0faafbdf5af92352d0ef729485a57ed0a
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-08-18 16:00:29 -0300

    Change completed task styling

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import './style.css';
 import 'font-awesome/css/font-awesome.css';
 
 class Task {
-  constructor (description, index, completed) {
+  constructor(description, index, completed) {
     this.description = description;
     this.index = index;
     this.completed = completed;
   }
 
   render() {
-    let taskContainer = document.createElement('li');
+    const taskContainer = document.createElement('li');
     taskContainer.classList.add('to-do-task');
     taskContainer.classList.add('app-entry');
 
-    let taskLeft = document.createElement('div');
+    const taskLeft = document.createElement('div');
     const taskCompleteButton = document.createElement('button');
 
     const taskDescription = document.createElement('p');
@@ -21,12 +21,11 @@ class Task {
     taskDescription.innerHTML = this.description;
 
     if (this.completed) {
-      taskCompleteButton.innerHTML =
-        '<i class="fa fa-check-square-o" aria-hidden="true"></i>';
+      taskCompleteButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
       taskDescription.classList.add('completed-task');
+      taskCompleteButton.classList.add('checked-task');
     } else {
-      taskCompleteButton.innerHTML =
-        '<i class="fa fa-square-o" aria-hidden="true"></i>';
+      taskCompleteButton.innerHTML = '<i class="fa fa-square-o" aria-hidden="true"></i>';
     }
 
     const taskDrag = document.createElement('p');
@@ -42,21 +41,20 @@ class Task {
   }
 }
 
-export const toDoUl = document.getElementById('to-do-list');
+const toDoUl = document.getElementById('to-do-list');
 
 const toDoList = [
   new Task('Bathe the dog', 1, false),
   new Task('Fix the sink', 3, false),
   new Task('Feed the kitty cat', 2, true),
-]
+];
 
 const renderList = (list, container) => {
   list.sort((task1, task2) => task1.index - task2.index)
-    .forEach(task => {
+    .forEach((task) => {
       const { html } = task.render();
-      console.log(html);
       container.appendChild(html);
     });
-}
+};
 
 renderList(toDoList, toDoUl);
